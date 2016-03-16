3109128bebf0c5400423d81d497ab2c5f17f6c7c
eaglesakura <eagle.sakura@gmail.com>
2016-03-16 13:51:26 +0900

    　戻り値例外付きのRunnable追加

---
+++ src/main/java/com/eaglesakura/util/ThrowableRunner.java
@@ -0,0 +1,44 @@
+package com.eaglesakura.util;
+
+/**
+ * Runnableをラップし、例外と戻り値を利用できるようにする。
+ */
+public class ThrowableRunner<ResultType, ErrorType extends Throwable> implements Runnable {
+    /**
+     * 投げられた例外
+     */
+    private ErrorType mError;
+
+    /**
+     * 処理の戻り値
+     */
+    private ResultType mResult;
+
+    private ThrowableRunnable<ResultType, ErrorType> mRunner;
+
+    public ThrowableRunner(ThrowableRunnable<ResultType, ErrorType> runner) {
+        mRunner = runner;
+    }
+
+    /**
+     * 処理を実行させる
+     */
+    @Override
+    public final void run() {
+        try {
+            mResult = mRunner.run();
+        } catch (Throwable e) {
+            mError = (ErrorType) e;
+        }
+    }
+
+    /**
+     * 値を取得するか例外を投げる
+     */
+    public ResultType getOrThrow() throws ErrorType {
+        if (mError != null) {
+            throw mError;
+        }
+        return mResult;
+    }
+}
