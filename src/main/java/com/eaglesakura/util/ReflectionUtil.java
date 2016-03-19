36f6799207400d72244b1ec6bd85365fa5809c08
eaglesakura <eagle.sakura@gmail.com>
2016-03-19 09:45:01 +0900

    Annotation系Utilとテストを追加

--- src/main/java/com/eaglesakura/util/ReflectionUtil.java
+++ src/main/java/com/eaglesakura/util/ReflectionUtil.java
@@ -1,16 +1,65 @@
 package com.eaglesakura.util;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * リフレクション関係のUtil
  */
 public class ReflectionUtil {
+
+    /**
+     * 指定したAnnotationが含まれたフィールド(public以外を含む)一覧を返す
+     *
+     * AnnotationにはRuntime属性が付与されてなければならない
+     */
+    public static <T extends Annotation> List<Field> listAnnotationFields(Class srcClass, Class<T> annotationClass) {
+        List<Field> result = new ArrayList<>();
+
+        while (!srcClass.equals(Object.class)) {
+            for (Field field : srcClass.getDeclaredFields()) {
+                T annotation = field.getAnnotation(annotationClass);
+                if (annotation != null) {
+                    result.add(field);
+                }
+            }
+
+            srcClass = srcClass.getSuperclass();
+        }
+
+        return result;
+    }
+
+    /**
+     * 指定したAnnotationが含まれたメソッド(public以外を含む)一覧を返す
+     *
+     * AnnotationにはRuntime属性が付与されてなければならない
+     * オーバーライドされたメソッドは1つにまとめて扱う
+     */
+    public static <T extends Annotation> List<Method> listAnnotationMethods(Class srcClass, Class<T> annotationClass) {
+        Map<String, Method> result = new HashMap<>();
+
+        while (!srcClass.equals(Object.class)) {
+            for (Method method : srcClass.getDeclaredMethods()) {
+                T annotation = method.getAnnotation(annotationClass);
+                if (annotation != null && !result.containsKey(method.getName())) {
+                    result.put(method.getName(), method);
+                }
+            }
+
+            srcClass = srcClass.getSuperclass();
+        }
+
+        return new ArrayList<>(result.values());
+    }
 
     /**
      * Enumのname()から実体を取得する。
--- src/test/java/com/eaglesakura/util/ReflectionUtilTest.java
+++ src/test/java/com/eaglesakura/util/ReflectionUtilTest.java
@@ -2,15 +2,46 @@ package com.eaglesakura.util;
 
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class ReflectionUtilTest {
+
+    @Test
+    public void 指定されたAnnotationのFieldを列挙する() {
+        List<Field> fields = ReflectionUtil.listAnnotationFields(BaseAnnotationClass.class, SampleAnnotation.class);
+        assertNotNull(fields);
+        assertEquals(fields.size(), 4);
+    }
+
+    @Test
+    public void 継承されたClassでFieldを列挙する() {
+        List<Field> fields = ReflectionUtil.listAnnotationFields(SubAnnotationClass.class, SampleAnnotation.class);
+        assertNotNull(fields);
+        assertEquals(fields.size(), 8);
+    }
+
+    @Test
+    public void 指定されたAnnotationのMethodを列挙する() {
+        List<Method> fields = ReflectionUtil.listAnnotationMethods(BaseAnnotationClass.class, SampleAnnotation.class);
+        assertNotNull(fields);
+        assertEquals(fields.size(), 4);
+    }
+
+    @Test
+    public void 継承されたClassでMethodを列挙する() {
+        List<Method> fields = ReflectionUtil.listAnnotationMethods(SubAnnotationClass.class, SampleAnnotation.class);
+        assertNotNull(fields);
+        assertEquals(fields.size(), 8);
+    }
 
     @Test
     public void インスタンス生成() {
@@ -40,4 +71,70 @@ public class ReflectionUtilTest {
         assertTrue(ReflectionUtil.isListInterface(LinkedList.class));
     }
 
+    public static class BaseAnnotationClass {
+        @SampleAnnotation
+        private int value0;
+
+        @SampleAnnotation
+        int value1;
+
+        @SampleAnnotation
+        protected int value2;
+
+        @SampleAnnotation
+        public int value3;
+
+        @SampleAnnotation
+        private void method0() {
+        }
+
+        @SampleAnnotation
+        void method1() {
+        }
+
+        @SampleAnnotation
+        protected void method2() {
+        }
+
+        @SampleAnnotation
+        public void method3() {
+        }
+
+    }
+
+    public static class SubAnnotationClass extends BaseAnnotationClass {
+        @SampleAnnotation
+        private int value4;
+
+        @SampleAnnotation
+        int value5;
+
+        @SampleAnnotation
+        protected int value6;
+
+        @SampleAnnotation
+        public int value7;
+
+        @SampleAnnotation
+        @Override
+        void method1() {
+        }
+
+        @SampleAnnotation
+        private void method5() {
+        }
+
+        @SampleAnnotation
+        void method6() {
+        }
+
+        @SampleAnnotation
+        protected void method7() {
+        }
+
+        @SampleAnnotation
+        public void method8() {
+        }
+
+    }
 }
